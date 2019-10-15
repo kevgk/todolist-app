947e9210a506e67c72d17578de0873ac899b5c1f
kevgk <kevgk@pm.me>
2019-10-15 21:41:15 +0200

    add spaces around destructured function args

--- src/components/TaskCountFeedback.js
+++ src/components/TaskCountFeedback.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function TaskCountFeedback({todoCount = 0}) {
+export default function TaskCountFeedback({ todoCount = 0 }) {
   return (
     <div className='container'>
       <span role="img" aria-label="Todo count feedback">{todoCount === 0 ? '😻' : '🙀'}</span>

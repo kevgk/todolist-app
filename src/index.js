dd953ea5c1faa6dade5962594ef071ff163d45a5
kevgk <kevgk@pm.me>
2019-10-16 22:24:57 +0200

    rename Tasklist component to TaskList

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import TodoList from './components/Todolist';
+import TodoList from './components/TodoList';
 import './styles.css';
 
 ReactDOM.render(<TodoList />, document.getElementById('root'));

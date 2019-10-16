636adb1ebb7ceabed14b888a7bb111e24194e302
kevgk <kevgk@pm.me>
2019-10-16 11:03:31 +0200

    add mockState

--- src/store/todo/reducer.test.js
+++ src/store/todo/reducer.test.js
@@ -1,28 +1,38 @@
 import { reducer } from './reducer';
 import { v4 as uuid } from 'uuid';
 
+let mockState;
+
+beforeEach(() => {
+  mockState = [
+    { id: uuid(), name: 'mock task 1', isChecked: false },
+    { id: uuid(), name: 'mock task 2', isChecked: true },
+    { id: uuid(), name: 'mock task 3', isChecked: false }
+  ];
+});
+
 describe('SET_TODO', () => {
   it('Sets todos to array, containing one object', () => {
     const payload = [{ id: 0, name: "test task"}];
-    const reduced = reducer([], { type: 'SET_TASKS', payload });
+    const reduced = reducer(mockState, { type: 'SET_TASKS', payload });
 
     expect(reduced).toEqual(payload);
   });
 
   it('Sets todos to array, containing multiple objects', () => {
     const payload = [
-      { id: 0, name: "test task 1"},
-      { id: 1, name: "test task 2"},
-      { id: 2, name: "test task 3"}
+      { id: uuid(), name: "test task 1"},
+      { id: uuid(), name: "test task 2"},
+      { id: uuid(), name: "test task 3"}
     ];
 
-    const reduced = reducer([], { type: 'SET_TASKS', payload });
+    const reduced = reducer(mockState, { type: 'SET_TASKS', payload });
 
     expect(reduced).toEqual(payload);
   });
 
   it('Set todos to empty array', () => {
-    const reduced = reducer([], {
+    const reduced = reducer(mockState, {
       type: 'SET_TASKS', payload: []
     });
 
@@ -48,14 +58,8 @@ describe('ADD_TASK', () => {
   });
 
   it('Add one task to populated state', () => {
-    const populatedState = [
-      { id: '0', name: 'test task 1', isChecked: false },
-      { id: '1', name: 'test task 2', isChecked: true },
-      { id: '2', name: 'test task 3', isChecked: false },
-    ];
-
     const name = "test task";
-    const reduced = reducer(populatedState, { type: 'ADD_TASK', payload: { id: uuid(), name } });
+    const reduced = reducer(mockState, { type: 'ADD_TASK', payload: { id: uuid(), name } });
 
     expect(reduced).toEqual(
       expect.arrayContaining([
@@ -71,31 +75,22 @@ describe('ADD_TASK', () => {
 
 describe('REMOVE_TASK', () => {
   it('Handle remove for nonexistent id on empty state', () => {
-    const reduced = reducer([], { type: 'REMOVE_TASK', payload: { id: '5' } });
+    const reduced = reducer([], { type: 'REMOVE_TASK', payload: { id: uuid() } });
     
     expect(reduced).toEqual([]);
   });
 
   it('Handle remove for nonexistent id on populated state', () => {
-    const populatedState = [
-      { id: uuid(), name: 'test task 1', isChecked: false },
-      { id: uuid(), name: 'test task 2', isChecked: false },
-    ];
-
-    const reduced = reducer(populatedState, { type: 'REMOVE_TASK', payload: { id: uuid() } });
+    const reduced = reducer(mockState, { type: 'REMOVE_TASK', payload: { id: uuid() } });
     
-    expect(reduced).toEqual(populatedState);
+    expect(reduced).toEqual(mockState);
   });
 
   it('Remove task', () => {
-    const idToRemove = uuid();
-    const populatedState = [
-      { id: uuid(), name: 'test task 1', isChecked: false },
-      { id: idToRemove, name: 'test task 2', isChecked: true },
-      { id: uuid(), name: 'test task 3', isChecked: false },
-    ];
-    
-    const reduced = reducer(populatedState, { type: ' REMOVE_TASK', payload: { id: idToRemove } });
+    const idToRemove = uuid();    
+    mockState.push({ id: idToRemove, name: 'test task 2', isChecked: true });
+
+    const reduced = reducer(mockState, { type: ' REMOVE_TASK', payload: { id: idToRemove } });
 
     expect(reduced).not.toContainEqual({id: idToRemove});
   });
@@ -104,27 +99,19 @@ describe('REMOVE_TASK', () => {
 describe('TOGGLE_TASK', () => {
   it('changes state from false to true', () => {
     const idToToggle = uuid();
-    const populatedState = [
-      { id: uuid(), name: 'test task 1', isChecked: false },
-      { id: idToToggle, name: 'test task 2', isChecked: false },
-      { id: uuid(), name: 'test task 3', isChecked: true },
-    ];
+    mockState.push({ id: idToToggle, name: 'test task 2', isChecked: false });
 
-    const reduced = reducer(populatedState, { type: 'TOGGLE_TASK', payload: { id: idToToggle } });
+    const reduced = reducer(mockState, { type: 'TOGGLE_TASK', payload: { id: idToToggle } });
 
-    expect(reduced[1].isChecked).toBe(true);
+    expect(reduced[mockState.length - 1].isChecked).toBe(true);
   });
 
   it('changes state from true to false', () => {
     const idToToggle = uuid();
-    const populatedState = [
-      { id: uuid(), name: 'test task 1', isChecked: false },
-      { id: idToToggle, name: 'test task 2', isChecked: true },
-      { id: uuid(), name: 'test task 3', isChecked: true },
-    ];
+    mockState.push({ id: idToToggle, name: 'test task 2', isChecked: true });
 
-    const reduced = reducer(populatedState, { type: 'TOGGLE_TASK', payload: { id: idToToggle } });
+    const reduced = reducer(mockState, { type: 'TOGGLE_TASK', payload: { id: idToToggle } });
 
-    expect(reduced[1].isChecked).toBe(false);
+    expect(reduced[mockState.length - 1].isChecked).toBe(false);
   });
 });

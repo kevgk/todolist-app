66590aef548944f3cf325d8366e6dfd0bfb5aa17
kevgk <kevgk@pm.me>
2019-10-16 10:12:32 +0200

    remove sideeffects

--- src/store/todo/reducer.js
+++ src/store/todo/reducer.js
@@ -1,27 +1,21 @@
-import localStorageSaveJSON from '../../utils/localStorageSaveJSON';
-
 export const reducer = (state, { type, payload }) => {
   switch(type) {
     case 'ADD_TODO': {
-      const newState = [
+      return [
         ...state, {
           id: payload.id,
           name: payload.name,
           isChecked: false
         }
       ];
-      return newState;
     }
     case 'REMOVE_TODO': {
-      const newState = state.filter(todo => todo.id !== payload.id);
-      localStorageSaveJSON('tasks', newState);
-      return newState;
+      return state.filter(todo => todo.id !== payload.id);
     }
     case 'TOGGLE_TODO': {
       const index = state.findIndex(todo => todo.id === payload.id);
       const newState = [...state];
       newState[index].isChecked = !state[index].isChecked;
-      localStorageSaveJSON('tasks', newState);
       return newState;
     }
     case 'SET_TODOS':

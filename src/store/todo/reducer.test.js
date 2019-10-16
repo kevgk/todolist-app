eca4faa45343c8d5ee2ff395e596ee5429e3fd33
kevgk <kevgk@pm.me>
2019-10-16 09:13:09 +0200

    add ADD_TODO tests

--- src/store/todo/reducer.test.js
+++ src/store/todo/reducer.test.js
@@ -28,3 +28,41 @@ describe('SET_TODO', () => {
     expect(reduced).toEqual([]);
   });
 });
+
+describe('ADD_TODO', () => {
+  it('Add one task to empty state', () => {
+    const name = "test task";
+    const reduced = reducer([], { type: 'ADD_TODO', payload: { name } });
+
+    expect(reduced).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          id: expect.any(String),
+          name,
+          isChecked: false
+        })
+      ])
+    );
+  });
+
+  it('Add one task to populated state', () => {
+    const populatedState = [
+      { id: '0', name: 'test task 1', isChecked: false },
+      { id: '1', name: 'test task 2', isChecked: true },
+      { id: '2', name: 'test task 3', isChecked: false },
+    ];
+    
+    const name = "test task";
+    const reduced = reducer(populatedState, { type: 'ADD_TODO', payload: { name } });
+
+    expect(reduced).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          id: expect.any(String),
+          name,
+          isChecked: false
+        })
+      ])
+    );
+  });
+});

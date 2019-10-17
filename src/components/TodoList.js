f28343082c3eae994ab7c9a15ec3134ee883fbf0
kevgk <kevgk@pm.me>
2019-10-17 07:06:09 +0200

    sort imports

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,10 @@
 import React, { useReducer, useEffect } from 'react';
 import { reducer } from '../store/reducer';
+import { getTasksFromLocalStorage } from '../store/actions';
 import TaskInput from './TaskInput';
 import TaskCountFeedback from './TaskCountFeedback';
 import TaskList from './TaskList';
 import localStorageSaveJSON from '../utils/localStorageSaveJSON';
-import { getTasksFromLocalStorage } from '../store/actions';
 import useSkipFirstRender from '../utils/useSkipFirstRender';
 
 export default function Todolist() {

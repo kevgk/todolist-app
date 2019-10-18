7ffbf5ea224595a1f2e487aec7ad7231c82efa0c
kevgk <kevgk@pm.me>
2019-10-18 07:02:15 +0200

    check name and value for undefined

--- src/utils/localStorageSaveJSON.js
+++ src/utils/localStorageSaveJSON.js
@@ -1,5 +1,5 @@
 export default function localStorageSaveJSON(name, value) {
-	if (name && value) {
+	if (typeof name !== 'undefined' && typeof value !== 'undefined') {
 		if (localStorage) {
 			localStorage.setItem(name, JSON.stringify(value));
 		}

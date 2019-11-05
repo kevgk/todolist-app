a45441370c6082cb1f03bdbe01651ce529ea9d81
kevgk <kevgk@pm.me>
2019-11-05 22:43:05 +0100

    add parameter types

--- src/utils/localStorageSaveJSON.ts
+++ src/utils/localStorageSaveJSON.ts
@@ -1,4 +1,4 @@
-export default function localStorageSaveJSON(name, value) {
+export default function localStorageSaveJSON(name: string, value: any) {
 	if (typeof name !== 'undefined' && typeof value !== 'undefined') {
 		if (localStorage) {
 			localStorage.setItem(name, JSON.stringify(value));
